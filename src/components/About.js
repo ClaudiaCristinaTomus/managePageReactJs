ff432316c3d9b4ed3608f85d31a250cfda340e48
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-02-11 17:48:15 +0200

    2nd commit

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./About.module.scss";
+
+const About = () => {
+  return (
+    <div className={styles.about}>
+        <div className={styles.left}>
+
+        </div>
+        <div className={styles.right}>
+            
+        </div>
+    </div>
+  )
+}
+
+export default About
